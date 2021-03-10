31bd4f343cbede9cf308712e0fcd84cb03eb8aa6
Noah Vawter <diydsp@yahoo.com>
2021-03-10 09:42:46 -0500

    added a super basic accelerating body example

--- realtime 01/main.cpp
+++ realtime 01/main.cpp
@@ -9,11 +9,29 @@ int main()
   std::cout << "Hello world!" << '\n';
 
   DynLoad dynload( "sub/libbasic1.so" );
+
   void *fp = dynload.fpget( "basic" );
   typedef int (func_ptr) (void);
   func_ptr *func;
   *(void **)(&func) = fp;
-  func();
+
+  void *fp2 = dynload.fpget( "func2" );
+  typedef int (func_ptr2) ( const float, float *);
+  func_ptr2 *func2;
+  *(void **)(&func2) = fp2;
+
+
+  int count;
+  float vel = 0.0f;
+  for( count = 0; count< 10; count++ ){
+    func2(  1.0f, &vel );
+    printf("%.1f\n", vel );
+  }
+  for( count = 0; count< 10; count++ ){
+    func2( -1.0f, &vel );
+    printf("%.1f\n", vel );
+  }
+
 
   return 0;
 }
--- realtime 01/sub/basic1.cpp
+++ realtime 01/sub/basic1.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
 extern "C"   void basic( void );
+//void basic( void );
 
 
 extern "C" {
+
 void basic( void )
 {
   std::cout << "hello subworld." << '\n';
 }
+
+
+  void func2( const float force, float *vel )
+  {
+    float mass = 0.25f;
+
+    *vel += force / mass;
+  }
+  
 }
