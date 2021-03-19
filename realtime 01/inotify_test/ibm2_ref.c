73296d9f5ea93cdb1c5f0e9bac8da237ef8ab87e
Noah Vawter <diydsp@yahoo.com>
2021-03-19 19:43:54 -0400

    this actually compiles with no errors, haven't run yet.

---
+++ realtime 01/inotify_test/ibm2_ref.c
@@ -0,0 +1,75 @@
+
+/*
+   This is reference code from an IBM developer website.  
+   accessed 3/19/2021 with very few mods to make it compile
+   under Ubuntu 18.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+
+#include <unistd.h>
+
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+int main( int argc, char *argv ) 
+{
+  int length, i = 0;
+  int fd;
+  int wd;
+  char buffer[BUF_LEN];
+
+  fd = inotify_init();
+
+  if ( fd < 0 ) {
+    perror( "inotify_init" );
+  }
+
+  wd = inotify_add_watch( fd, "/home/diydsp", 
+                         IN_MODIFY | IN_CREATE | IN_DELETE );
+  length = read( fd, buffer, BUF_LEN );  
+
+  if ( length < 0 ) {
+    perror( "read" );
+  }  
+
+  while ( i < length ) {
+    struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
+    if ( event->len ) {
+      if ( event->mask & IN_CREATE ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "The directory %s was created.\n", event->name );       
+        }
+        else {
+          printf( "The file %s was created.\n", event->name );
+        }
+      }
+      else if ( event->mask & IN_DELETE ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "The directory %s was deleted.\n", event->name );       
+        }
+        else {
+          printf( "The file %s was deleted.\n", event->name );
+        }
+      }
+      else if ( event->mask & IN_MODIFY ) {
+        if ( event->mask & IN_ISDIR ) {
+          printf( "The directory %s was modified.\n", event->name );
+        }
+        else {
+          printf( "The file %s was modified.\n", event->name );
+        }
+      }
+    }
+    i += EVENT_SIZE + event->len;
+  }
+
+  ( void ) inotify_rm_watch( fd, wd );
+  ( void ) close( fd );
+
+  exit( 0 );
+}
