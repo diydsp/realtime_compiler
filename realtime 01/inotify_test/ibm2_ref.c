c8df7dfa1d259049988a6bb21ed92a007e1f7ab0
Noah Vawter <diydsp@yahoo.com>
2021-03-19 20:00:01 -0400

    now reads argv[1] for target filename/dirname.  briefly tested.  sees some events

--- realtime 01/inotify_test/ibm2_ref.c
+++ realtime 01/inotify_test/ibm2_ref.c
@@ -12,11 +12,12 @@
 #include <sys/inotify.h>
 
 #include <unistd.h>
+#include <string.h>
 
 #define EVENT_SIZE  ( sizeof (struct inotify_event) )
 #define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
 
-int main( int argc, char *argv ) 
+int main( int argc, const char *argv[] ) 
 {
   int length, i = 0;
   int fd;
@@ -29,7 +30,11 @@ int main( int argc, char *argv )
     perror( "inotify_init" );
   }
 
-  wd = inotify_add_watch( fd, "/home/diydsp", 
+  char name[8000];
+  strcpy(name,argv[1]);
+  //const *name = argv;
+  wd = inotify_add_watch( fd, name,
+  //wd = inotify_add_watch( fd, "/home/diydsp", 
                          IN_MODIFY | IN_CREATE | IN_DELETE );
   length = read( fd, buffer, BUF_LEN );  
 
